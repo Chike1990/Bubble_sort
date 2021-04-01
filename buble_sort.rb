4786ff5be258e8a1fdb0c1e93a67f125c810189f
Genius8 <erasmusokiror111@gmail.com>
2021-04-01 14:36:52 +0300

    Finish the project

--- buble_sort.rb
+++ buble_sort.rb
@@ -2,7 +2,11 @@ def bubble_sort(array)
     return array if array.length <= 1
     0.upto(array.length-1) do |j|
         0.upto(array.length-2-j) do |i|
+            array[i],array[i+1]=array[i+1],array[i]if array[i]>array[i+1]
         
         
     end
 end
+array
+end
+p bubble_sort([100,20,1,9,5,2,100,0,18,12,11])
